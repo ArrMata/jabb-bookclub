d94aaa75da6f8f9df13edcc8a1e282ae2cff2989
Arrington Mata <59297501+ArrMata@users.noreply.github.com>
2023-09-26 12:10:55 -0600

    Update env.js

--- bookclub.client/src/env.js
+++ bookclub.client/src/env.js
@@ -1,8 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
-export const audience = 'https://localhost:8888'
-export const clientId = '7TkwLlEwyOaG8jqhrymJz1GHkaAVsCdx'
+export const domain = 'dev-p7vx5zxzsmz52nqh.us.auth0.com'
+export const audience = 'bcwpersonal.com'
+export const clientId = 'mL5Vkm043yVJ4zUIDW65Mk9ovb8WQB4G'
 export const gbak = 'AIzaSyBcf7Ry8H9mWxSzyfghzaS7sU0WN9Qxtj4'
 export const ntak = 'NhL89IMsqCIzG7YABO2ybDtxKek7Mh1L'
